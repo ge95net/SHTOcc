6f9d0425069878ba1f7b53c6344b009ee320a636
qiuchengyu <qiuchenyu2-c@my.cityu.edu.hk>
2025-05-15 12:14:49 +0800

    COTR

---
+++ SHTOcc-COTR/train_eval_occ.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+export PYTHONUNBUFFERED=1
+export TORCH_DISTRIBUTED_DEBUG=DETAIL
+CONFIG=$1
+GPUS=$2
+arr=(${CONFIG//// })
+
+bash tools/dist_train_occ.sh $CONFIG $GPUS --auto-resume
+for i in {24..1}
+do
+bash tools/dist_test_occ.sh work_dirs/${arr[-1]:0:${#arr[-1]}-3}/${arr[-1]:0:${#arr[-1]}-3}.py work_dirs/${arr[-1]:0:${#arr[-1]}-3}/epoch_${i}_ema.pth $GPUS --eval mIoU
+done
